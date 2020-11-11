13cc4f065a9b6b749083bf0dee148a5fad427703
Valentin <valentin.furmanek@gmail.com>
2020-11-11 16:05:01 +0100

    Add title when multiple files

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 18:00:48 by vfurmane          #+#    #+#             */
-/*   Updated: 2020/11/11 15:55:45 by vfurmane         ###   ########.fr       */
+/*   Updated: 2020/11/11 16:03:00 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,27 @@
 #include "put.h"
 #include "maze.h"
 
+void	ft_print_title(char *title, int i)
+{
+	if (i > 1)
+		ft_putstr("\n");
+	ft_putstr("===== ");
+	ft_putstr(title);
+	ft_putstr(" =====");
+	ft_putstr("\n\n");
+}
+
 void	ft_prepare_solve(t_coord cursor, t_maze config, int **maze)
 {
-	cursor.x = config.end.x;
-	cursor.y = config.end.y;
-	ft_solve(maze, 1, config, cursor);
-	ft_print_maze(maze, config);
+	if (maze)
+	{
+		cursor.x = config.end.x;
+		cursor.y = config.end.y;
+		ft_solve(maze, 1, config, cursor);
+		ft_print_maze(maze, config);
+	}
+	else
+		ft_putstr("Error\n");
 }
 
 void	ft_free_maze(int **maze, t_maze config)
@@ -48,13 +63,10 @@ int		main(int argc, char **argv)
 	continue_solve = 1;
 	while (continue_solve)
 	{
+		if (argc >= 2)
+			ft_print_title(argv[i], i);
 		maze = ft_get_maze(argc >= 2 ? argv[i] : NULL, &config);
-		if (maze)
-		{
-			ft_prepare_solve(cursor, config, maze);
-		}
-		else
-			ft_putstr("Error\n");
+		ft_prepare_solve(cursor, config, maze);
 		i++;
 		if (argc == 1)
 			continue_solve = 0;
